ab910f6cd6b46d1c45bc6cc3f628bb108854bcfb
Millie Cheung <cheungmillie9@gmail.com>
2022-04-06 15:42:40 +0200

    completed get and post

--- routes/menu.js
+++ routes/menu.js
@@ -1,9 +1,22 @@
 import express from "express";
+import { readFile } from "fs/promises";
+import { v4 as uuidv4 } from "uuid";
+
+export const menu = JSON.parse(
+  await readFile(new URL("../menuData.json", import.meta.url))
+);
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("hi"));
-router.post("/", (req, res) => console.log("post"));
+router.get("/", (req, res) => res.send(menu));
+
+router.post("/", (req, res) => {
+  const newItem = req.body;
+  const newItemWithId = { ...newItem, id: uuidv4() };
+  menu.push(newItemWithId);
+  res.send(`${newItem.title} has been added!`);
+});
+
 router.put("/:id", (req, res) => console.log("put"));
 router.post("/:id", (req, res) => console.log("delete"));
 
