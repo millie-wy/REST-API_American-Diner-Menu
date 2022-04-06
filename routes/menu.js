c4a22b10c12f1fafd41f5f5775e02318a4d8b493
Millie Cheung <cheungmillie9@gmail.com>
2022-04-06 19:13:36 +0200

    added delete (but havent text yet)

--- routes/menu.js
+++ routes/menu.js
@@ -8,16 +8,25 @@ export const menu = JSON.parse(
 
 const router = express.Router();
 
+// get the full menu from database
 router.get("/", (req, res) => res.send(menu));
 
+// add a new item to the database
 router.post("/", (req, res) => {
   const newItem = req.body;
   const newItemWithId = { ...newItem, id: uuidv4() };
   menu.push(newItemWithId);
-  res.send(`${newItem.title} has been added!`);
+  res.send(`${newItem.title} has been added! 👏`);
 });
 
+// update an item in the database ?
 router.put("/:id", (req, res) => console.log("put"));
-router.post("/:id", (req, res) => console.log("delete"));
+
+// delete an item from the database
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  menu = menu.filter((item) => item.id !== id);
+  res.send(`${newItem.title} has been deleted! 👌`);
+});
 
 export default router;
