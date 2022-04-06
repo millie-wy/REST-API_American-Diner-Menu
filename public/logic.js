15c7682cbfaca9ae8db6baf44c83c8c4d6bbb862
Millie Cheung <cheungmillie9@gmail.com>
2022-04-06 15:41:38 +0200

    removed the old code as they have been copied to the jsx (but jsx not working yet)

--- public/logic.js
+++ public/logic.js
@@ -1,1 +1,16 @@
-console.log("logic js linked");
+/*
+document.getElementById("openmenu").addEventListener("click", async (event) => {
+  let menu = await loadMenu();
+  console.log(menu);
+});
+
+async function loadMenu() {
+  console.log("click");
+  try {
+    let response = await fetch("http://localhost:3000/api/menu");
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+*/
