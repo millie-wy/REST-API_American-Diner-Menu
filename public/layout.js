beae5fe58b7793c83f95f945c9dcea710c7418b1
Millie Cheung <cheungmillie9@gmail.com>
2022-04-06 19:13:06 +0200

    used a newer version of react (but data is not showing)

--- public/layout.js
+++ public/layout.js
@@ -1,29 +1,87 @@
-var Layout = function Layout() {
-  //const [data, setData] = useState([]);
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-  var loadMenu = function loadMenu() {
-    console.log("click");
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var _React = React,
+    useState = _React.useState,
+    useEffect = _React.useEffect;
+
+
+var Layout = function Layout() {
+  var _useState = useState([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      menu = _useState2[0],
+      setMenu = _useState2[1];
+
+  useEffect(function () {
     fetch("http://localhost:3000/api/menu").then(function (response) {
       return response.json();
     }).then(function (jsonData) {
-      return console.log(jsonData);
+      setMenu([].concat(_toConsumableArray(menu), [jsonData]));
     }).catch(function (err) {
       return console.log(err);
     });
-  };
+  }, []);
 
   return React.createElement(
     "div",
-    null,
+    {
+      style: {
+        background: "yellow",
+        height: "200px",
+        width: "600px"
+      }
+    },
     React.createElement(
       "h1",
       null,
       "HELLO FROM REACT"
     ),
+    menu.map(function (items) {
+      return items.map(function (item) {
+        if (item.category === "Burger") {
+          console.log(item.title, item.description, item.price);
+          React.createElement(
+            "div",
+            {
+              key: item.id,
+              style: {
+                height: "200px",
+                width: "200px",
+                background: "skyblue"
+              }
+            },
+            React.createElement(
+              "p",
+              null,
+              item.title
+            ),
+            React.createElement(
+              "p",
+              null,
+              item.description
+            ),
+            React.createElement(
+              "p",
+              null,
+              item.price
+            )
+          );
+        } else {
+          console.log("ELSE: " + item.title, item.description, item.price);
+        }
+      });
+    }),
     React.createElement(
-      "button",
-      { onClick: loadMenu },
-      "Open Menu"
+      "p",
+      null,
+      "End?"
     )
   );
 };
+
+var container = document.getElementById("root");
+var root = ReactDOM.createRoot(container);
+root.render(React.createElement(Layout, null));
+
+// ReactDOM.render(<Layout />, document.querySelector("#root"));
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,18 +1,57 @@
-const Layout = () => {
-  //const [data, setData] = useState([]);
+const { useState, useEffect } = React;
 
-  const loadMenu = () => {
-    console.log("click");
+const Layout = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
     fetch("http://localhost:3000/api/menu")
       .then((response) => response.json())
-      .then((jsonData) => console.log(jsonData))
+      .then((jsonData) => {
+        setMenu([...menu, jsonData]);
+      })
       .catch((err) => console.log(err));
-  };
+  }, []);
 
-  return (
-    <div>
+  return menu.length === 0 ? (
+    <h1>nothing to show ????</h1>
+  ) : (
+    <div
+      style={{
+        background: "yellow",
+        height: "200px",
+        width: "600px",
+      }}
+    >
       <h1>HELLO FROM REACT</h1>
-      <button onClick={loadMenu}>Open Menu</button>
+      {/* <button onClick={loadMenu}>Open Menu</button> */}
+      {menu.map((items) =>
+        items.map((item) => {
+          if (item.category === "Burger") {
+            console.log(item.title, item.description, item.price);
+            <div
+              key={item.id}
+              style={{
+                height: "200px",
+                width: "200px",
+                background: "skyblue",
+              }}
+            >
+              <p>{item.title}</p>
+              <p>{item.description}</p>
+              <p>{item.price}</p>
+            </div>;
+          } else {
+            console.log("ELSE: " + item.title, item.description, item.price);
+          }
+        })
+      )}
+      <p>End?</p>
     </div>
   );
 };
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Layout />);
+
+// ReactDOM.render(<Layout />, document.querySelector("#root"));
