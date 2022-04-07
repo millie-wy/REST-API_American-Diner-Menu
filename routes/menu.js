53b263958a7b27be1f472cd1adc23f321762fd77
Millie Cheung <cheungmillie9@gmail.com>
2022-04-07 12:09:54 +0200

    finished get and post and added several functions

--- routes/menu.js
+++ routes/menu.js
@@ -1,32 +1,93 @@
 import express from "express";
-import { readFile } from "fs/promises";
+import fs from "fs";
+import { readFile, writeFile } from "fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
-export const menu = JSON.parse(
-  await readFile(new URL("../menuData.json", import.meta.url))
-);
+// export const menu = JSON.parse(
+//   await readFile(new URL("../menuData.json", import.meta.url))
+// );
 
 const router = express.Router();
 
 // get the full menu from database
-router.get("/", (req, res) => res.send(menu));
+router.get("/", (req, res) => res.send(getMenu()));
 
 // add a new item to the database
 router.post("/", (req, res) => {
+  let menuTobeUpdated = getMenu();
   const newItem = req.body;
   const newItemWithId = { ...newItem, id: uuidv4() };
-  menu.push(newItemWithId);
+  menuTobeUpdated.push(newItemWithId);
+  saveMenu(menuTobeUpdated);
   res.send(`${newItem.title} has been added! 👏`);
 });
 
-// update an item in the database ?
-router.put("/:id", (req, res) => console.log("put"));
+/*
+// retrieve an specific item from the database
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  //find
+  const item = menu.filter((item) => item.id === id);
+  res.send(item);
+});
+*/
 
-// delete an item from the database
+// update an item in the database ?
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  let menuToBeAdjusted = getMenu().map((item) => {
+    if (item.id == id) {
+      return req.body;
+    }
+    return item;
+  });
+  saveMenu(menuToBeAdjusted);
+  // req.body;
+  // req.params.id;
+});
+/*
+// delete an item from the database  *** "error": "Assignment to constant variable."
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
+  let currentMenu = getMenu();
+  if (!doesMenuExist(id)) {
+    res.status(404).send("does not exists.");
+  }
+  /* Delete the menu with ID from currentMenu */
+//saveMenu();
+
+/* const { id } = req.params;
   menu = menu.filter((item) => item.id !== id);
-  res.send(`${newItem.title} has been deleted! 👌`);
-});
+  res.send(`${newItem.title} has been deleted! 👌`); */
+
+//});
 
 export default router;
+
+function getMenu() {
+  let menuData = fs.readFileSync("./menuData.json");
+  let menu = JSON.parse(menuData);
+  return menu;
+}
+
+function saveMenu(updatedMenu) {
+  fs.writeFileSync(
+    "./menuData.json",
+    JSON.stringify(updatedMenu, null, 2),
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+    }
+  );
+}
+
+function doesMenuExist(id) {
+  let currentMenu = getMenu();
+  let foundMenu = currentMenu.find(() => {});
+  if (foundMenu) {
+    return true;
+  } else {
+    return false;
+  }
+}
