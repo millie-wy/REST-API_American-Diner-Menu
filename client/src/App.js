94a4348a072aa40f1723ada6783c3fd96b45268c
Millie Cheung <cheungmillie9@gmail.com>
2022-04-09 14:25:59 +0200

    moved code from app to menu jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,125 +1,20 @@
-import { useState, useEffect } from "react";
-import "./app.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Create from "./components/Create";
+import Layout from "./components/Layout";
+import Menu from "./components/Menu";
 
 function App() {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/menu")
-      .then((response) => response.json())
-      .then((jsonData) => {
-        setMenu([...menu, jsonData]);
-      })
-      .catch((err) => console.log(err));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return menu.length === 0 ? (
-    <h1>Loading</h1>
-  ) : (
-    <div id="menu">
-      <div className="stripe s-top" />
-      <div className="stripe s-bottom" />
-      <div className="logo">
-        <h1 className="brand">
-          American
-          <br />
-          <span className="star">
-            ★<span className="diner">Diner</span>★
-          </span>
-        </h1>
-      </div>
-      <div className="menu-content">
-        <div className="burger-section">
-          <div className="section-heading">
-            <h2 className="heading-text">Burgers</h2>
-          </div>
-          <div className="section-content">
-            {menu.map((items) =>
-              items.map((item) =>
-                item.category === "Burger" ? (
-                  <div className="menu-item" key={item.id}>
-                    <div className="menu-item-title">
-                      <p>{item.title}</p>
-                      <p>{item.price}</p>
-                    </div>
-                    <p className="menu-item-description">{item.description}</p>
-                  </div>
-                ) : null
-              )
-            )}
-          </div>
-        </div>
-
-        <div className="steak-section">
-          <div className="section-heading">
-            <h2 className="heading-text">Steaks & Ribs</h2>
-          </div>
-          <div className="section-content">
-            {menu.map((items) =>
-              items.map((item) =>
-                item.category === "Steak or Ribs" ? (
-                  <div className="menu-item" key={item.id}>
-                    <div className="menu-item-title">
-                      <p>{item.title}</p>
-                      <p>{item.price}</p>
-                    </div>
-                    <p className="menu-item-description">{item.description}</p>
-                  </div>
-                ) : null
-              )
-            )}
-          </div>
-        </div>
-        <div className="sections-right">
-          <div className="sweets-section">
-            <div className="section-heading">
-              <h2 className="heading-text">Sweets</h2>
-            </div>
-            <div className="section-content">
-              {menu.map((items) =>
-                items.map((item) =>
-                  item.category === "Sweets" ? (
-                    <div className="menu-item" key={item.id}>
-                      <div className="menu-item-title">
-                        <p>{item.title}</p>
-                        <p>{item.price}</p>
-                      </div>
-                      <p className="menu-item-description">
-                        {item.description}
-                      </p>
-                    </div>
-                  ) : null
-                )
-              )}
-            </div>
-          </div>
-          <div className="drinks-section">
-            <div className="section-heading">
-              <h2 className="heading-text">Drinks</h2>
-            </div>
-            <div className="section-content">
-              {menu.map((items) =>
-                items.map((item) =>
-                  item.category === "Drinks" ? (
-                    <div className="menu-item" key={item.id}>
-                      <div className="menu-item-title">
-                        <p>{item.title}</p>
-                        <p>{item.price}</p>
-                      </div>
-                      <p className="menu-item-description">
-                        {item.description}
-                      </p>
-                    </div>
-                  ) : null
-                )
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <button onClick={loadMenu}>Open Menu</button> */}
-    </div>
+  // return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Menu />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/menu/:id" element="menu item" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
