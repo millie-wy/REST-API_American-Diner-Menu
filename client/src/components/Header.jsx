5c22e707089e9caf72446679cfb201265541aa22
Millie Cheung <cheungmillie9@gmail.com>
2022-04-09 14:26:27 +0200

    created header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { faUtensils, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div style={headerStyle}>
+      <Link style={linkStyle} to="/">
+        <FontAwesomeIcon style={iconStyle} icon={faUtensils} />
+        MENU
+      </Link>
+      <Link style={linkStyle} to="/create">
+        <FontAwesomeIcon style={iconStyle} icon={faPlus} />
+        ADD
+      </Link>
+    </div>
+  );
+};
+
+const headerStyle = {
+  margin: "20px",
+  textAlign: "center",
+};
+
+const linkStyle = {
+  padding: "20px",
+  fontSize: "20px",
+  fontWeight: "bold",
+  textDecoration: "none",
+  color: "#eee",
+};
+
+const iconStyle = {
+  paddingRight: "10px",
+  "&:hover": {
+    color: "blue",
+  },
+};
+
+export default Header;
