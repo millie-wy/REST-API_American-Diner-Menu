2a0d52756dede475aee70c0340a9a50528895089
Millie Cheung <cheungmillie9@gmail.com>
2022-04-09 15:37:41 +0200

    inserted single menu item and added function to helper

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Create from "./components/Create";
 import Layout from "./components/Layout";
 import Menu from "./components/Menu";
+import SingleMenuItem from "./components/SingleMenuItem";
 
 function App() {
   // return <div></div>;
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Menu />} />
           <Route path="/create" element={<Create />} />
-          <Route path="/menu/:id" element="menu item" />
+          <Route path="/menu/:id" element={<SingleMenuItem />} />
         </Route>
       </Routes>
     </BrowserRouter>
