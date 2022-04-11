881ab4657ed8675626a9e39fe32361878af6912e
Millie Cheung <cheungmillie9@gmail.com>
2022-04-11 16:18:44 +0200

    added states to show the post status

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -6,8 +6,11 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [price, setPrice] = useState(0);
   const [description, setDescription] = useState("");
+  const [isAdded, setIsAdded] = useState(false);
+  const [postStatus, setPostStatus] = useState("");
 
   const addItem = async (e) => {
+    setIsAdded(true);
     e.preventDefault();
     let newItem = {
       category: category,
@@ -17,114 +20,124 @@ const Create = () => {
     };
 
     let status = await makeRequest("/api/menu", "POST", newItem);
-    alert(status);
+    setPostStatus(status);
+    setTimeout(() => {
+      setIsAdded(false);
+      setPostStatus("");
+    }, 2000);
   };
 
   return (
     <div style={rootStyle}>
       <div style={headerStyle}>
         <h1 style={{ lineHeight: "25px" }}>
-          <span style={{ color: "#0e2636" }}>★</span> Create new item
+          <span style={{ color: "#0e2636" }}>★</span> Add new item
           <span style={{ color: "#0e2636" }}> ★</span>
         </h1>
       </div>
-      <form style={formStyle} onSubmit={addItem}>
-        <div style={radioContainerStyle}>
-          <input
-            type="radio"
-            name="category"
-            value="Burgers"
-            required
-            onChange={(e) => setCategory(e.currentTarget.value)}
-          />
-          <label style={radioLabelStyle} name="burgers">
-            Burgers
-          </label>
+      {isAdded ? (
+        <div style={{ height: "340px", textAlign: "center" }}>
+          <h2 style={{ lineHeight: "280px" }}>{postStatus}</h2>
+        </div>
+      ) : (
+        <form style={formStyle} onSubmit={addItem}>
+          <div style={radioContainerStyle}>
+            <input
+              type="radio"
+              name="category"
+              value="Burgers"
+              required
+              onChange={(e) => setCategory(e.currentTarget.value)}
+            />
+            <label style={radioLabelStyle} name="burgers">
+              Burgers
+            </label>
 
-          <input
-            type="radio"
-            name="category"
-            value="Steak or Ribs"
-            required
-            onChange={(e) => setCategory(e.currentTarget.value)}
-          />
-          <label style={radioLabelStyle} name="steak">
-            Steak or Ribs
-          </label>
-          <input
-            type="radio"
-            name="category"
-            value="Sweets"
-            required
-            onChange={(e) => setCategory(e.currentTarget.value)}
-          />
-          <label style={radioLabelStyle} name="sweets">
-            Sweets
-          </label>
-          <input
-            type="radio"
-            name="category"
-            value="Drinks"
-            required
-            onChange={(e) => setCategory(e.currentTarget.value)}
-          />
-          <label style={radioLabelStyle} name="drinks">
-            Drinks
-          </label>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            textAlign: "left",
-            gap: "20px",
-          }}
-        >
-          <label style={labelStyle}>
-            Title <br />
             <input
-              style={inputStyle}
-              type="text"
-              name="title"
+              type="radio"
+              name="category"
+              value="Steak or Ribs"
               required
-              onChange={(e) => setTitle(e.currentTarget.value)}
+              onChange={(e) => setCategory(e.currentTarget.value)}
             />
-          </label>
-          <label style={labelStyle}>
-            Price
-            <br />
+            <label style={radioLabelStyle} name="steak">
+              Steak or Ribs
+            </label>
             <input
-              style={inputStyle}
-              type="number"
-              name="price"
+              type="radio"
+              name="category"
+              value="Sweets"
               required
-              onChange={(e) => setPrice(e.currentTarget.value)}
+              onChange={(e) => setCategory(e.currentTarget.value)}
             />
-          </label>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            textAlign: "left",
-          }}
-        >
-          <label style={labelStyle}>
-            Description <br />
-            <textarea
-              rows="4"
-              style={{ ...inputStyle, width: "300px" }}
-              type="text"
-              name="description"
+            <label style={radioLabelStyle} name="sweets">
+              Sweets
+            </label>
+            <input
+              type="radio"
+              name="category"
+              value="Drinks"
               required
-              onChange={(e) => setDescription(e.currentTarget.value)}
+              onChange={(e) => setCategory(e.currentTarget.value)}
             />
-          </label>
-        </div>
-        <button style={buttonStyle} type="submit">
-          Submit
-        </button>
-      </form>
+            <label style={radioLabelStyle} name="drinks">
+              Drinks
+            </label>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "left",
+              gap: "20px",
+            }}
+          >
+            <label style={labelStyle}>
+              Title <br />
+              <input
+                style={inputStyle}
+                type="text"
+                name="title"
+                required
+                onChange={(e) => setTitle(e.currentTarget.value)}
+              />
+            </label>
+            <label style={labelStyle}>
+              Price
+              <br />
+              <input
+                style={inputStyle}
+                type="number"
+                name="price"
+                required
+                onChange={(e) => setPrice(e.currentTarget.value)}
+              />
+            </label>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "left",
+            }}
+          >
+            <label style={labelStyle}>
+              Description <br />
+              <textarea
+                rows="4"
+                style={{ ...inputStyle, width: "300px" }}
+                type="text"
+                name="description"
+                required
+                onChange={(e) => setDescription(e.currentTarget.value)}
+              />
+            </label>
+          </div>
+          <button style={buttonStyle} type="submit">
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 };
@@ -170,6 +183,7 @@ const formStyle = {
   textAlign: "left",
   display: "flex",
   flexDirection: "column",
+  minHeight: "340px",
 };
 
 const labelStyle = {
